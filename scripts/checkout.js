10715f3461d11da019e66f28ea6d49575c10f832
Daria <daria.lysunets03@gmail.com>
2024-09-11 21:28:54 +0100

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,13 +2,53 @@ import { renderOrderSummary } from '../scripts/checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 // import '../data/cart-class.js'
 // import '../data/backend-practice.js';
 // import '../data/car.js'
 
-loadProducts(()=>{
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+    resolve('value1');
+    });
+    }),
+    new Promise  ((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    })
+
+]).then((values) => {
   renderOrderSummary();
   renderPaymentSummary();
-  
+  renderCheckoutHeader(); 
 });
-renderCheckoutHeader(); 
+
+/*
+new Promise((resolve) => {
+loadProducts(() => {
+resolve('value1');
+});
+
+}).then((value) => {
+return new Promise  ((resolve) => {
+  loadCart(() => {
+    resolve();
+  });
+}); 
+
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+  renderCheckoutHeader(); 
+});
+
+loadProducts(()=>{
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader(); 
+  });
+});*/
+  
