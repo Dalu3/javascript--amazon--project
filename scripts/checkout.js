738046b2975de84865ad63273011574cb0db160d
Daria <daria.lysunets03@gmail.com>
2024-09-11 20:44:11 +0100

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,14 @@
 import { renderOrderSummary } from '../scripts/checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
+import { loadProducts } from '../data/products.js';
 // import '../data/cart-class.js'
-import '../data/backend-practice.js';
+// import '../data/backend-practice.js';
 // import '../data/car.js'
 
-
-renderOrderSummary();
-renderPaymentSummary();
-renderCheckoutHeader();
+loadProducts(()=>{
+  renderOrderSummary();
+  renderPaymentSummary();
+  
+});
+renderCheckoutHeader(); 
